c96f39fa241c2309ff20a6c1bcb0c45647fc5c26
MarcosCarrizoA <mcarrizoaslanian@mail.austral.edu.ar>
2025-10-16 13:12:30 -0300

    wip
    wip multiplayer

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Fusion;
+using Fusion.Sockets;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameNetworkManager : MonoBehaviour, INetworkRunnerCallbacks
+{
+    private NetworkRunner runner;
+
+    async void Start()
+    {
+        runner = gameObject.AddComponent<NetworkRunner>();
+        runner.ProvideInput = true;
+
+        // Inicia el juego como Host (puede ser Client si cambiás el modo)
+        await runner.StartGame(new StartGameArgs
+        {
+            GameMode = GameMode.Host,
+            SessionName = "SmashTanksRoom",
+            Scene = SceneRef.FromIndex(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex),
+            SceneManager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+
+    // Se llama cuando entra un jugador nuevo
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        Debug.Log($"Jugador conectado: {player.PlayerId}");
+
+        var tankPrefab = Resources.Load<GameObject>("Tank");
+        if (tankPrefab != null)
+        {
+            runner.Spawn(tankPrefab, Vector3.zero, Quaternion.identity, player);
+        }
+        else
+        {
+            Debug.LogError("No se encontró el prefab Tank en Resources/");
+        }
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        var data = new NetworkInputData
+        {
+            Horizontal = Input.GetAxisRaw("Horizontal"),
+            Vertical = Input.GetAxisRaw("Vertical")
+        };
+        input.Set(data);
+    }
+
+    // Callbacks que no usamos todavía:
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remote, NetConnectFailedReason reason) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason reason) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
+
+}
+
+// Estructura de input para enviar a la red
+public struct NetworkInputData : INetworkInput
+{
+    public float Horizontal;
+    public float Vertical;
+}
