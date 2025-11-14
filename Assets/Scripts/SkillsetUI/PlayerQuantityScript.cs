652bbd14f51fc0fcc203448b54786c30ce2b8690
MarcosCarrizoA <mcarrizoaslanian@mail.austral.edu.ar>
2025-11-14 12:26:31 -0300

    Player Quantity
    Select Player Quantity

---
+++ Assets/Scripts/SkillsetUI/PlayerQuantityScript.cs
@@ -0,0 +1,65 @@
+﻿using Manager;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SkillsetUI
+{
+    public class PlayerQuantityScript : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private Button confirmButton;
+        [SerializeField] private Button increaseButton;
+        [SerializeField] private Button decreaseButton;
+        [SerializeField] private TMP_Text playerCountText;
+        
+        [Header("Players")]
+        [SerializeField] private int minPlayers = 2;
+        [SerializeField] private int maxPlayers = 8;
+        [SerializeField] private int currentPlayers = 2;
+        
+        private void Start()
+        {
+            increaseButton.onClick.AddListener(IncreasePlayers);
+            decreaseButton.onClick.AddListener(DecreasePlayers);
+            confirmButton.onClick.AddListener(() =>
+            {
+                GameManagerScript.Instance.SetPlayerCount(currentPlayers);
+                GameManagerScript.Instance.ShowSkillsetScreen();
+            });
+            decreaseButton.interactable = false;
+        }
+        
+        private void IncreasePlayers()
+        {
+            if (currentPlayers < maxPlayers)
+            {
+                playerCountText.text = (++currentPlayers).ToString();
+                SetButtonsInteractable(true, true);
+                if (currentPlayers == maxPlayers)
+                {
+                    SetButtonsInteractable(false, true);
+                }
+            }
+        }
+        
+        private void DecreasePlayers()
+        {
+            if (currentPlayers > minPlayers)
+            {
+                playerCountText.text = (--currentPlayers).ToString();
+                SetButtonsInteractable(true, true);
+                if (currentPlayers == minPlayers)
+                {
+                   SetButtonsInteractable(true, false);
+                }
+            }
+        }
+
+        private void SetButtonsInteractable(bool canIncrease, bool canDecrease)
+        {
+            if (increaseButton) increaseButton.interactable = canIncrease;
+            if (decreaseButton) decreaseButton.interactable = canDecrease;
+        }
+    }
+}
